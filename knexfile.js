23be8e4c4e9da8456e22d75655c8713c47ffc76f
OwenOrcan <owenorcan@gmail.com>
2024-06-03 23:43:30 -0400

    Change the main knex config name to "Production" and added a local database called "development"
    Signed-off-by: OwenOrcan <owenorcan@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 
 const knexConfig = {
-  development: {
-    client: 'mysql2',
+  production: {
+    client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
@@ -10,22 +10,21 @@ const knexConfig = {
       database: process.env.DB_NAME,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
-  test: {
-    client: 'sqlite3',
+  development: {
+    client: "mysql2",
     connection: {
-      filename: './test.sqlite'
+      host: process.env.DEV_DB_HOST,
+      user: process.env.DEV_DB_USER,
+      password: process.env.DEV_DB_PASSWORD,
+      database: process.env.DEV_DB_NAME,
     },
-    useNullAsDefault: true,
     migrations: {
-      directory: './migrations'
+      tableName: "knex_migrations",
     },
-    seeds: {
-      directory: './seeds'
-    }
-  }
+  },
 };
 
 export default knexConfig;
